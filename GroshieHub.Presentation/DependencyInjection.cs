5c629ab10862243e9599a77aadd03ad1268122f9
Prog1910 <102432993+Prog1910@users.noreply.github.com>
2025-01-24 22:39:50 +0400

    feat: add project template
    The template includes Swagger and Serilog configurations

---
+++ GroshieHub.Presentation/DependencyInjection.cs
@@ -0,0 +1,59 @@
+using Audit.Core;
+using Audit.Http;
+using Microsoft.OpenApi.Models;
+
+namespace GroshieHub.Presentation;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.ConfigureCors();
+		services.ConfigureSerilog();
+		services.ConfigureSwagger();
+
+		return services;
+	}
+
+	private static void ConfigureSerilog(this IServiceCollection services)
+	{
+		Configuration.Setup().UseSerilog(config =>
+			config.Message(auditEvent =>
+			{
+				if (auditEvent is not AuditEventHttpClient httpClientEvent) return auditEvent.ToJson();
+
+				var contentBody = httpClientEvent.Action?.Response?.Content?.Body;
+				if (contentBody is not string { Length: > 1000 } stringBody) return auditEvent.ToJson();
+
+				var responseContent = httpClientEvent.Action!.Response?.Content;
+				if (responseContent is not null) responseContent.Body = stringBody[..1000] + "<...>";
+				return auditEvent.ToJson();
+			}));
+	}
+
+	private static void ConfigureCors(this IServiceCollection services)
+	{
+		services.AddCors(options =>
+			options.AddPolicy("CorsPolicy", builder =>
+				builder.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader()
+					.WithExposedHeaders("X-Pagination"))
+		);
+	}
+
+	private static void ConfigureSwagger(this IServiceCollection services)
+	{
+		services.AddSwaggerGen(options =>
+		{
+			options.SwaggerDoc("v1", new OpenApiInfo
+			{
+				Version = "v1",
+				Title = "Groshie Hub Public API"
+			});
+
+			var xmlPath = $"{typeof(Program).Assembly.GetName().Name}.xml";
+			options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlPath));
+		});
+	}
+}
---
+++ GroshieHub.Presentation/Formatters/JsonPatchFormatter.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+
+namespace GroshieHub.Presentation.Formatters;
+
+public static class JsonPatchFormatter
+{
+	public static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter() =>
+		new ServiceCollection().AddLogging().AddMvc().AddNewtonsoftJson()
+			.Services.BuildServiceProvider().GetRequiredService<IOptions<MvcOptions>>()
+			.Value.InputFormatters.OfType<NewtonsoftJsonPatchInputFormatter>().First();
+}
