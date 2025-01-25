7c9d6fc4c1be2c182890f976be7dac45a5ad108e
Prog1910 <102432993+Prog1910@users.noreply.github.com>
2025-01-25 23:19:53 +0400

    feat: log not NotFound exceptions

--- GroshieHub.Presentation/Extensions/GlobalErrorsHandler.cs
+++ GroshieHub.Presentation/Extensions/GlobalErrorsHandler.cs
@@ -1,4 +1,4 @@
-using GroshieHub.Domain.Exceptions;
+﻿using GroshieHub.Domain.Exceptions;
 using GroshieHub.Domain.Exceptions.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -19,20 +19,32 @@ public sealed class GlobalErrorsHandler : IExceptionFilter
 
 		var statusCode = error switch
 		{
+			InvalidRequestException => StatusCodes.Status422UnprocessableEntity,
 			NotFoundException => StatusCodes.Status404NotFound,
 			ApiRequestLimitException => StatusCodes.Status429TooManyRequests,
 			BadRequestException => StatusCodes.Status400BadRequest,
-			_ => StatusCodes.Status500InternalServerError
+			_ => StatusCodes.Status500InternalServerError,
 		};
 
 		string? detail = null;
 
-		context.Result = new ObjectResult(new ProblemDetails
+		if (statusCode == StatusCodes.Status422UnprocessableEntity)
 		{
-			Status = statusCode,
-			Title = error.Message,
-			Detail = detail
-		});
+			detail = "For more information, see documentation: https://currencyapi.com/docs/status-codes#_422";
+		}
+		if (statusCode != StatusCodes.Status404NotFound)
+		{
+			Log.Error(error.ToString());
+		}
+
+		context.Result = new ObjectResult(
+			new ProblemDetails
+			{
+				Status = statusCode,
+				Title = error.Message,
+				Detail = detail,
+			}
+		);
 
 		context.ExceptionHandled = true;
 	}
